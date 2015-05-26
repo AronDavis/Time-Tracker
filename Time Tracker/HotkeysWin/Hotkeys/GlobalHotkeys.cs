264fc8f99912b50ed24924409dd1b374c597eb99
Aron Davis <arontd@gmail.com>
2015-05-26 16:34:48 -0400

    Comments/GUI/Cleanup

--- Time Tracker/HotkeysWin/Hotkeys/GlobalHotkeys.cs
+++ Time Tracker/HotkeysWin/Hotkeys/GlobalHotkeys.cs
@@ -2,20 +2,24 @@
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
-namespace Hotkeys
+namespace TimeTracker.Hotkeys
 {
     public class GlobalHotkey
     {
         private int modifier;
-        private int key;
+        public int key { get; private set; }
         private IntPtr hWnd;
         private int id;
+        public bool ctrl { get; private set; }
+        public bool alt { get; private set; }
 
-        public GlobalHotkey(int modifier, Keys key)
+        public GlobalHotkey(bool ctrl, bool alt, Keys key)
         {
-            this.modifier = modifier;
+            this.ctrl = ctrl;
+            this.alt = alt;
+            this.modifier = (ctrl ? HKConstants.CTRL : 0) + (alt ? HKConstants.ALT : 0);
             this.key = (int)key;
-            this.hWnd = form.Handle;
+            this.hWnd = GlobalData.mainForm.Handle;
             id = this.GetHashCode();
         }
 
@@ -39,5 +43,10 @@ namespace Hotkeys
 
         [DllImport("user32.dll")]
         private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        public override string ToString()
+        {
+            return (ctrl ? "Ctrl + " : "") + (alt ? "Alt + " : "") + ((Keys)key).ToString();
+        }
     }
 }
