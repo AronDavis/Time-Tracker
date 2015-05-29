7489d494d9ff717ada4bd74eaba1ace010f5eba2
Aron Davis <arontd@gmail.com>
2015-05-29 16:16:38 -0400

    Adding accidentally excluded files.

---
+++ Time Tracker/TimeTracker/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimeTracker
+{
+    public class Category
+    {
+        public String ID;
+        public String DisplayText;
+
+        public Category(string id, string displayText)
+        {
+            ID = id;
+            DisplayText = displayText;
+        }
+
+        public override string ToString()
+        {
+            return DisplayText;
+        }
+    }
+}
---
+++ Time Tracker/TimeTracker/IssueData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TimeTracker
+{
+    public class IssueData
+    {
+        public String ID;
+        public String DisplayText;
+        public Category Category;
+        public TimeSpan TodaysLoggedTime;
+
+        public IssueData(String id, string displayText, Category category, TimeSpan todaysLoggedTime)
+        {
+            ID = id;
+            DisplayText = displayText;
+            Category = category;
+            TodaysLoggedTime = todaysLoggedTime;
+        }
+    }
+}
