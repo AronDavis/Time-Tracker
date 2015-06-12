54e77556c4275bf321170af0188fcaa123e3438f
Carlos Vasquez <hispanasian@gmail.com>
2015-06-12 14:26:03 -0400

    additions for issue-#10

--- Time Tracker/TimeTracker/TimeUtility.cs
+++ Time Tracker/TimeTracker/TimeUtility.cs
@@ -14,6 +14,14 @@ namespace TimeTracker
             Hours
         }
 
+        public enum RoundDirection
+        {
+            Up,
+            Average,
+            Down
+        }
+
+        //TODO: Refactor to use the RoundTo method
         public static TimeSpan Round(TimeSpan time, int increment, RoundIncrementMode mode)
         {
             TimeSpan roundTo;
@@ -38,6 +46,34 @@ namespace TimeTracker
 
             return new TimeSpan(roundedTicks);
         }
+        
+        public static TimeSpan Round(TimeSpan time, int increment, RoundIncrementMode mode, RoundDirection direction)
+        {
+            TimeSpan roundedTime;
+            TimeSpan roundTo = RoundTo(increment, mode);
+            long roundedTicks;
+
+            switch(direction)
+            {
+                case RoundDirection.Up:
+                    roundedTicks = (long)Math.Ceiling((double)(time.Ticks) / roundTo.Ticks);
+                    roundedTime = new TimeSpan(roundedTicks);
+                    break;
+                case RoundDirection.Average:
+                    roundedTime = Round(time, increment, mode);
+                    break;
+                case RoundDirection.Down:
+                    roundedTicks = (long)Math.Floor((double)(time.Ticks) / roundTo.Ticks);
+                    roundedTime = new TimeSpan(roundedTicks);
+                    break;
+                default:
+                    // Default to average
+                    roundedTime = Round(time, increment, mode);
+                    break;
+            }
+
+            return roundedTime;
+        }
 
         //TODO: add level of precision?
         public static String FormatRoundedTime(TimeSpan time, RoundIncrementMode mode)
@@ -53,6 +89,35 @@ namespace TimeTracker
                 default:
                     return "Error";
             }
+        }
+
+        /// <summary>
+        /// Returns the TimeSpan that should be rounded to based on the increment and mode
+        /// </summary>
+        /// <param name="increment"></param>
+        /// <param name="mode"></param>
+        /// <returns>the TimeSpan that should be rounded to based on the increment and mode</returns>
+        private static TimeSpan RoundTo(int increment, RoundIncrementMode mode)
+        {
+            TimeSpan roundTo;
+
+            switch (mode)
+            {
+                case RoundIncrementMode.Seconds:
+                    roundTo = new TimeSpan(0, 0, increment);
+                    break;
+                case RoundIncrementMode.Minutes:
+                    roundTo = new TimeSpan(0, increment, 0);
+                    break;
+                case RoundIncrementMode.Hours:
+                    roundTo = new TimeSpan(increment, 0, 0);
+                    break;
+                default:
+                    roundTo = new TimeSpan();
+                    break;
+            }
+
+            return roundTo;
         }
     }
 }
