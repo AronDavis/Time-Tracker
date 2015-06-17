6488c4925b7a2d1b200d3fa29f84130e5f070835
Aron Davis <arontd@gmail.com>
2015-06-17 16:23:18 -0400

    More column support (with grid)

---
+++ Time Tracker/TimeTracker/ucTimeSummary.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace TimeTracker
+{
+    partial class ucTimeSummary
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTimeSummary = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTimeSummary)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTimeSummary
+            // 
+            this.dgvTimeSummary.AllowUserToAddRows = false;
+            this.dgvTimeSummary.AllowUserToDeleteRows = false;
+            this.dgvTimeSummary.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvTimeSummary.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTimeSummary.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvTimeSummary.Location = new System.Drawing.Point(0, 0);
+            this.dgvTimeSummary.Name = "dgvTimeSummary";
+            this.dgvTimeSummary.Size = new System.Drawing.Size(511, 150);
+            this.dgvTimeSummary.TabIndex = 0;
+            // 
+            // ucTimeSummary
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dgvTimeSummary);
+            this.Name = "ucTimeSummary";
+            this.Size = new System.Drawing.Size(511, 150);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTimeSummary)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataGridView dgvTimeSummary;
+
+    }
+}
---
+++ Time Tracker/TimeTracker/ucTimeSummary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TimeTracker
+{
+    public partial class ucTimeSummary : UserControl
+    {
+        DataTable table;
+        public ucTimeSummary()
+        {
+            InitializeComponent();
+        }
+
+        public void Initialize()
+        {
+            table = new DataTable("TotalTime");
+            table.Columns.Add(new DataColumn("Category"));
+            table.Columns.Add(new DataColumn("Total Time"));
+            table.Columns.Add(new DataColumn("Time Rounded"));
+            for (int i = 0; i < GlobalData.Categories.Count; i++)
+            {
+                DataRow row = table.NewRow();
+                row.SetField<Category>(0, GlobalData.Categories[i]);
+                row.SetField<TimeSpan>(1, new TimeSpan());
+                row.SetField<TimeSpan>(2, new TimeSpan());
+                table.Rows.Add(row);
+            }
+
+            dgvTimeSummary.DataSource = table;
+        }
+
+        public void Update()
+        {
+
+            TimeSpan[] categoryTimeSums = new TimeSpan[GlobalData.Categories.Count];
+
+            for (int i = 0; i < GlobalData.Issues.Count; i++)
+            {
+                Category cat = GlobalData.Issues[i].Category;
+
+                int catIndex = GlobalData.Categories.IndexOf(cat);
+                categoryTimeSums[catIndex] += GlobalData.Issues[i].TodaysLoggedTime;
+            }
+
+            
+            for(int i = 0; i < categoryTimeSums.Length; i++)
+            {
+                DataRow row = table.Select(String.Format("Category = '{0}'", GlobalData.Categories[i]))[0];
+                row.SetField<TimeSpan>(1, categoryTimeSums[i]);
+                row.SetField<TimeSpan>(2, TimeUtility.Round(categoryTimeSums[i], GlobalData.RoundTo, GlobalData.RoundDirection));
+            }
+        }
+    }
+}
